bcd92f50c040437b533c3a3a9682183b74bfb6b2
Phan Hoang Tuan Minh <20020302@vnu.edu.vn>
2022-03-15 23:33:29 +0700

    troll

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Harupip");
         System.out.println("bebunbeokute");
+        System.out.println("minhvertu");
     }
 }
