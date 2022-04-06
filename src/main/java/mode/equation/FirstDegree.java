b52970ebf857712546d2b3f66273229a647602ea
Harupip <chudangnghia2002@gmail.com>
2022-04-07 01:17:28 +0700

    Change sth in Conversion

--- src/main/java/mode/equation/FirstDegree.java
+++ src/main/java/mode/equation/FirstDegree.java
@@ -19,29 +19,88 @@ public class FirstDegree extends Degree {
     }
     // >= ax + b >= 0 => x >= -b/a
     public void notSmaller (Fraction a, Fraction b) {
-        if ( a.toDouble() > 0) {
-            solutions.add((MathUtil.divide(MathUtil.multiply(new Fraction(-1),b), a)).simplify());
-            System.out.print("[,+∞");
-        }
-    }
-
-    public void solveInEqual(Fraction a, Fraction b) {
         if (a.toDouble() == 0) {
 //            throw Exception;
             System.out.println("a cannot be zero");
+            System.exit(0);
         }
-
+        Fraction x =  (MathUtil.divide(MathUtil.multiply(new Fraction(-1),b), a)).simplify();
+        if ( a.toDouble() > 0) {
+            System.out.print("[" + x.toDouble() + ", +∞)");
+        }
+        else {
+            System.out.print("(-∞," + x.toDouble() + "]");
+        }
     }
 
+    public void notGreater (Fraction a, Fraction b) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+            System.exit(0);
+        }
+        Fraction x =  (MathUtil.divide(MathUtil.multiply(new Fraction(-1),b), a)).simplify();
+        if ( a.toDouble() > 0) {
+            System.out.print("(-∞," + x.toDouble() + "]");
+        }
+        else {
+            System.out.print("[" + x.toDouble() + ", +∞)");
+
+        }
+    }
+
+    public void smaller (Fraction a, Fraction b) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+            System.exit(0);
+        }
+        Fraction x =  (MathUtil.divide(MathUtil.multiply(new Fraction(-1),b), a)).simplify();
+        if ( a.toDouble() > 0) {
+            System.out.print("(-∞," + x.toDouble() + ")");
+        }
+        else {
+            System.out.print("(" + x.toDouble() + ", +∞)");
+
+        }
+    }
+
+    public void greater (Fraction a, Fraction b) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+            System.exit(0);
+        }
+        Fraction x =  (MathUtil.divide(MathUtil.multiply(new Fraction(-1),b), a)).simplify();
+        if ( a.toDouble() > 0) {
+            System.out.print("(" + x.toDouble() + ", +∞)");
+        }
+        else {
+            System.out.print("(-∞," + x.toDouble() + ")");
+        }
+    }
+
+
+//    public void solveInEqual(Fraction a, Fraction b) {
+//        if (a.toDouble() == 0) {
+////            throw Exception;
+//            System.out.println("a cannot be zero");
+//        }
+//
+//    }
+
     public static void main(String[] args) {
-        Fraction a = new Fraction(66);
+        Fraction a = new Fraction(0);
         Fraction b = new Fraction(3);
         FirstDegree test = new FirstDegree();
         test.notSmaller(a,b);
-        for (Fraction solution: test.solutions) {
-            System.out.print(solution.toDouble());
 
-        }
+
+
+//        for (Fraction solution: test.solutions) {
+//            System.out.print(solution.toDouble());
+//
+//        }
 
     }
 }
