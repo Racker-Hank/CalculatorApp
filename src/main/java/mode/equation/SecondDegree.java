11581d32474136a3d8f9fbd640596098ccdc4515
Harupip <chudangnghia2002@gmail.com>
2022-04-07 01:17:28 +0700

    Change sth in Conversion

--- src/main/java/mode/equation/SecondDegree.java
+++ src/main/java/mode/equation/SecondDegree.java
@@ -19,7 +19,7 @@ public class SecondDegree extends Degree {
             else if (Δ.toDouble() > 0) {
                 Fraction x = new Fraction(Δ.toDouble());
                 x.numerator = Math.sqrt(x.numerator);
-                x.denominator = Math.sqrt(x.denominator);
+//                x.denominator = Math.sqrt(x.denominator);
 
                 solutions.add(MathUtil.divide(MathUtil.add(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a)));
                 solutions.add(MathUtil.divide(MathUtil.subtract(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a)));
@@ -27,15 +27,131 @@ public class SecondDegree extends Degree {
         }
     }
 
+    public void Smaller (Fraction a, Fraction b, Fraction c) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+        }
+        else {
+            Fraction Δ = MathUtil.subtract(MathUtil.multiply(b,b),MathUtil.multiply(MathUtil.multiply(new Fraction(4),a),c));
 
+            if (Δ.toDouble() == 0) {
+                Fraction x = MathUtil.divide(MathUtil.multiply(new Fraction(-1),b),MathUtil.multiply(new Fraction(2),a));
+                System.out.println("(-∞," + x.toDouble() + ") U (" + x.toDouble() + ", +∞)");
+            }
+
+            else if (Δ.toDouble() > 0) {
+                Fraction x = new Fraction(Δ.toDouble());
+                x.numerator = Math.sqrt(x.numerator);
+
+                Fraction x2 = MathUtil.divide(MathUtil.add(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                Fraction x1 = MathUtil.divide(MathUtil.subtract(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                if (a.toDouble() > 0) {
+                    System.out.println("(" + x1.toDouble() + ", " + x2.toDouble() + ")");
+                }
+                else {
+                    System.out.println("(-∞, " + x2.toDouble() + ") U (" + x1.toDouble() + ", +∞)");
+                }
+            }
+        }
+    }
+
+    public void Greater (Fraction a, Fraction b, Fraction c) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+        }
+        else {
+            Fraction Δ = MathUtil.subtract(MathUtil.multiply(b,b),MathUtil.multiply(MathUtil.multiply(new Fraction(4),a),c));
+
+            if (Δ.toDouble() == 0) {
+                Fraction x = MathUtil.divide(MathUtil.multiply(new Fraction(-1),b),MathUtil.multiply(new Fraction(2),a));
+                System.out.println("(-∞," + x.toDouble() + ") U (" + x.toDouble() + ", +∞)");
+            }
+
+            else if (Δ.toDouble() > 0) {
+                Fraction x = new Fraction(Δ.toDouble());
+                x.numerator = Math.sqrt(x.numerator);
+
+                Fraction x2 = MathUtil.divide(MathUtil.add(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                Fraction x1 = MathUtil.divide(MathUtil.subtract(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                if (a.toDouble() > 0) {
+                    System.out.println("(-∞, " + x1.toDouble() + ") U (" + x2.toDouble() + ", +∞)");
+
+                }
+                else {
+                    System.out.println("(" + x2.toDouble() + ", " + x1.toDouble() + ")");
+                }
+            }
+        }
+    }
+
+    public void notSmaller (Fraction a, Fraction b, Fraction c) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+        }
+        else {
+            Fraction Δ = MathUtil.subtract(MathUtil.multiply(b,b),MathUtil.multiply(MathUtil.multiply(new Fraction(4),a),c));
+
+            if (Δ.toDouble() == 0) {
+                Fraction x = MathUtil.divide(MathUtil.multiply(new Fraction(-1),b),MathUtil.multiply(new Fraction(2),a));
+                System.out.println("(-∞," + x.toDouble() + "] U [" + x.toDouble() + ", +∞)");
+            }
+
+            else if (Δ.toDouble() > 0) {
+                Fraction x = new Fraction(Δ.toDouble());
+                x.numerator = Math.sqrt(x.numerator);
+
+                Fraction x2 = MathUtil.divide(MathUtil.add(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                Fraction x1 = MathUtil.divide(MathUtil.subtract(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                if (a.toDouble() > 0) {
+                    System.out.println("(-∞, " + x1.toDouble() + "] U [" + x2.toDouble() + ", +∞)");
+
+                }
+                else {
+                    System.out.println("[" + x2.toDouble() + ", " + x1.toDouble() + "]");
+                }
+            }
+        }
+    }
+
+    public void notGreater (Fraction a, Fraction b, Fraction c) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+        }
+        else {
+            Fraction Δ = MathUtil.subtract(MathUtil.multiply(b,b),MathUtil.multiply(MathUtil.multiply(new Fraction(4),a),c));
+
+            if (Δ.toDouble() == 0) {
+                Fraction x = MathUtil.divide(MathUtil.multiply(new Fraction(-1),b),MathUtil.multiply(new Fraction(2),a));
+                System.out.println("(-∞," + x.toDouble() + "] U [" + x.toDouble() + ", +∞)");
+            }
+
+            else if (Δ.toDouble() > 0) {
+                Fraction x = new Fraction(Δ.toDouble());
+                x.numerator = Math.sqrt(x.numerator);
+
+                Fraction x2 = MathUtil.divide(MathUtil.add(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                Fraction x1 = MathUtil.divide(MathUtil.subtract(MathUtil.multiply(new Fraction(-1),b),x),MathUtil.multiply(new Fraction(2),a));
+                if (a.toDouble() > 0) {
+                    System.out.println("[" + x1.toDouble() + ", " + x2.toDouble() + "]");
+                }
+                else {
+                    System.out.println("(-∞, " + x2.toDouble() + "] U [" + x1.toDouble() + ", +∞)");
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SecondDegree x = new SecondDegree();
         x.solveEqual(new Fraction(1),new Fraction(2),new Fraction(1));
+        x.notGreater(new Fraction(1),new Fraction(-5),new Fraction(4));
 
-
-        for (Fraction solution: x.solutions) {
-            System.out.println(solution.toDouble());
-        }
+//        for (Fraction solution: x.solutions) {
+//            System.out.println(solution.toDouble());
+//        }
     }
 }
--- src/main/java/mode/equation/ThirdDegree.java
+++ src/main/java/mode/equation/ThirdDegree.java
@@ -41,17 +41,21 @@ public class ThirdDegree extends Degree {
             if ( Δ.toDouble() > 0) {
                 Fraction k = MathUtil.divide(numeratorOfK, denominatorOfK);
                 if ( Math.abs(k.toDouble()) <= 1) {
-                    Fraction x1 = new Fraction ((2 * Math.sqrt(Δ.toDouble())
-                            * Math.cos(Math.acos(k.toDouble())/3)- b.toDouble()) / (3 * a.toDouble()));
+                    Fraction x1 = new Fraction((2 * Math.sqrt(Δ.toDouble())
+                            * Math.cos(Math.acos(k.toDouble()) / 3) - b.toDouble()) / (3 * a.toDouble()));
                     Fraction x2 = new Fraction((2 * Math.sqrt(Δ.toDouble())
-                            * Math.cos(Math.acos(k.toDouble())/3 - (2 * Math.PI / 3))  - b.toDouble()) / (3 * a.toDouble()));
+                            * Math.cos(Math.acos(k.toDouble()) / 3 - (2 * Math.PI / 3)) - b.toDouble()) / (3 * a.toDouble()));
                     Fraction x3 = new Fraction((2 * Math.sqrt(Δ.toDouble())
-                            * Math.cos(Math.acos(k.toDouble())/3 + (2 * Math.PI / 3))  - b.toDouble()) / (3 * a.toDouble()));
+                            * Math.cos(Math.acos(k.toDouble()) / 3 + (2 * Math.PI / 3)) - b.toDouble()) / (3 * a.toDouble()));
                     solutions.add(x1);
-                    solutions.add(x2);
-                    solutions.add(x3);
+                    if (x2.toDouble() != x3.toDouble()) {
+                        solutions.add(x2);
+                        solutions.add(x3);
+                    }
+                    else {
+                        solutions.add(x2);
+                    }
                 }
-
                 else if (Math.abs(k.toDouble()) > 1) {
                     double leftx = (Math.sqrt(Δ.toDouble()) * Math.abs(k.toDouble())) / (3 * a.toDouble() * k.toDouble());
                     double middlex = MathUtil.root(Math.abs(k.toDouble())+ Math.sqrt((k.toDouble() * k.toDouble())-1),3)
@@ -80,12 +84,77 @@ public class ThirdDegree extends Degree {
         }
     }
 
+    public void greater (Fraction a, Fraction b, Fraction c, Fraction d) {
+        if (a.toDouble() == 0) {
+//            throw Exception;
+            System.out.println("a cannot be zero");
+        }
+        else {
+            Fraction Δ = MathUtil.subtract(MathUtil.multiply(b,b),MathUtil.multiply(MathUtil.multiply(new Fraction(3),a),c));
+
+            Fraction asd = roota(hat(as(Δ.toDouble()).toDouble(),3).toDouble(),2);
+            Fraction denominatorOfK = MathUtil.multiply(new Fraction(2),asd);
+
+            Fraction ve1ofNum = MathUtil.multiply(MathUtil.multiply(MathUtil.multiply(new Fraction(9),a),b),c);
+            Fraction ve2ofNum = MathUtil.multiply(new Fraction(2),hat(b.toDouble(),3));
+            Fraction ve3ofNum = MathUtil.multiply(MathUtil.multiply(new Fraction(27),d),hat(a.toDouble(),2));
+            Fraction numeratorOfK = MathUtil.subtract(MathUtil.subtract(ve1ofNum,ve2ofNum),ve3ofNum);
+
+            if ( Δ.toDouble() > 0) {
+                Fraction k = MathUtil.divide(numeratorOfK, denominatorOfK);
+                if ( Math.abs(k.toDouble()) <= 1) {
+                    Fraction x1 = new Fraction ((2 * Math.sqrt(Δ.toDouble())
+                            * Math.cos(Math.acos(k.toDouble())/3) - b.toDouble()) / (3 * a.toDouble()));
+                    Fraction x2 = new Fraction((2 * Math.sqrt(Δ.toDouble())
+                            * Math.cos(Math.acos(k.toDouble())/3 - (2 * Math.PI / 3))  - b.toDouble()) / (3 * a.toDouble()));
+                    Fraction x3 = new Fraction((2 * Math.sqrt(Δ.toDouble())
+                            * Math.cos(Math.acos(k.toDouble())/3 + (2 * Math.PI / 3))  - b.toDouble()) / (3 * a.toDouble()));
+                    if (x2.toDouble() == x3.toDouble()) {
+                        System.out.println("(" + x1.toDouble() + ", +∞)");
+                    }
+                    else {
+                        System.out.println("(" + x3.toDouble() + ", " + x2.toDouble() + ") U (" + x1.toDouble() + ", +∞)");
+                    }
+                }
+
+                else if (Math.abs(k.toDouble()) > 1) {
+                    double leftx = (Math.sqrt(Δ.toDouble()) * Math.abs(k.toDouble())) / (3 * a.toDouble() * k.toDouble());
+                    double middlex = MathUtil.root(Math.abs(k.toDouble())+ Math.sqrt((k.toDouble() * k.toDouble())-1),3)
+                            + MathUtil.root(Math.abs(k.toDouble()) - Math.sqrt((k.toDouble() * k.toDouble())-1),3);
+                    double rightx = -(b.toDouble()) / (3 * a.toDouble());
+                    Fraction no = new Fraction((leftx * middlex) + rightx);
+
+                    System.out.println("(" + no.toDouble() + "+∞)");
+
+                }
+            }
+
+            else if (Δ.toDouble() == 0) {
+                double n1 = (-b.toDouble() + MathUtil.root(b.toDouble() * b.toDouble() * b.toDouble()
+                        - 27 * d.toDouble() * a.toDouble() * a.toDouble(),3)) / (3 *a.toDouble());
+
+                System.out.println("(" + n1 + ", +∞");
+            }
+
+            else if ( Δ.toDouble() < 0) {
+                Fraction k = MathUtil.divide(numeratorOfK, denominatorOfK);
+                double leftx = Math.sqrt(Math.abs(Δ.toDouble())) / (3 * a.toDouble());
+                double middlex = MathUtil.root(k.toDouble() + Math.sqrt((k.toDouble() * k.toDouble()) + 1),3)
+                        + MathUtil.root(k.toDouble() - Math.sqrt((k.toDouble() * k.toDouble())+1),3);
+                double rightx = b.toDouble() / (3 * a.toDouble());
+                Fraction no1 = new Fraction((leftx * middlex) - rightx);
+
+                System.out.println("(" + no1.toDouble() + "+∞)");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ThirdDegree a = new ThirdDegree();
-        a.solveEqual(new Fraction(1),new Fraction(3), new Fraction(3), new Fraction(1));
-        for (Fraction solution: a.solutions) {
-            System.out.println(solution.toDouble());
-        }
+        a.greater(new Fraction(1),new Fraction(-4), new Fraction(5), new Fraction(-2));
+//        for (Fraction solution: a.solutions) {
+//            System.out.println(solution.toDouble());
+//        }
     }
 
 }
