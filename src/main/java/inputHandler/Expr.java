81a94ff626f753ae6cd72cb3808cfa2737ea193f
sreyalSgniK <doviethung2002@gmail.com>
2022-05-07 20:10:06 +0700

    fix constants and add rand function

--- src/main/java/inputHandler/Expr.java
+++ src/main/java/inputHandler/Expr.java
@@ -1,5 +1,7 @@
 package inputHandler;
 
+import UI.StandardController;
+import mode.standard.Standard;
 import operation.Constants;
 
 import java.lang.reflect.Field;
@@ -115,14 +117,15 @@ public class Expr {
     private static final byte  // values for code array; values >= 0 are indices into constants array
             PLUS = -1, MINUS = -2, TIMES = -3, DIVIDE = -4, POWER = -5, SIN = -6, COS = -7, TAN = -8,
             COT = -9, SEC = -10, CSC = -11, ARCSIN = -12, ARCCOS = -13, ARCTAN = -14, EXP = -15,
-            LN = -16, LOG10 = -17, LOG2 = -18, ABS = -19, SQRT = -20, ROOT = -21, MODULO = -22,
-            FACTORIAL = -23, PERCENT = -24, UNARYMINUS = -25, VARIABLE = -26;
+            LN = -16, LOG10 = -17, LOG2 = -18, ABS = -19, SQRT = -20, ROOT = -21, MODULO = -22, RANDOM = -23,
+            RANDOM_INT = -24,
+            FACTORIAL = -25, PERCENT = -26, UNARYMINUS = -27, VARIABLE = -28;
 
 
     private static final String[] functionNames = {  // names of standard functions, used during parsing
             "sin", "cos", "tan", "cot", "sec",
             "csc", "arcsin", "arccos", "arctan", "exp",
-            "ln", "log10", "log2", "abs", "sqrt", "rt", "mod"};
+            "ln", "log10", "log2", "abs", "sqrt", "rt", "mod", "rand", "randint"};
 
     private static final List<Constants.Constant> constantsList = new ArrayList<>(); // array of constants
 
@@ -161,18 +164,19 @@ public class Expr {
                 } else {
                     double x = stack[--top];
                     double ans = Double.NaN;
+                    boolean deg_rad = StandardController.toggleSwitch.switchOnProperty().get();
                     switch (code[i]) {
                         case SIN:
-                            ans = Math.sin(x);
+                            ans = deg_rad ? MathUtil.sinDegrees(x) : MathUtil.sinRadians(x);
                             break;
                         case COS:
-                            ans = Math.cos(x);
+                            ans = deg_rad ? MathUtil.cosDegrees(x) : MathUtil.cosRadians(x);
                             break;
                         case TAN:
-                            ans = Math.tan(x);
+                            ans = deg_rad ? MathUtil.tanDegrees(x) : MathUtil.tanRadians(x);
                             break;
                         case COT:
-                            ans = Math.cos(x) / Math.sin(x);
+                            ans = deg_rad ? MathUtil.cotDegrees(x) : MathUtil.cotRadians(x);
                             break;
                         case SEC:
                             ans = 1.0 / Math.cos(x);
@@ -223,6 +227,14 @@ public class Expr {
                         case MODULO:
                             y = stack[--top];
                             ans = y % x;
+                            break;
+                        case RANDOM:
+                            y = stack[--top];
+                            ans = MathUtil.randInRange(y,x,false);
+                            break;
+                        case RANDOM_INT:
+                            y = stack[--top];
+                            ans = MathUtil.randInRange(y,x,true);
                             break;
                     }
                     if (Double.isNaN(ans))
@@ -494,15 +506,18 @@ public class Expr {
 //        System.out.println(Arrays.toString(expr3.code));
 //        System.out.println(Arrays.toString(expr3.constants));
 
-        Expr test = new Expr("π");
-        System.out.println(test.value(0));
-        test = new Expr("cos(π)");
-        System.out.println(test.value(0));
-        test = new Expr("e");
-        System.out.println(test.value(0));
-        test = new Expr("qp");
-        System.out.println(test.value(0));
-        test = new Expr("h1");
+//        Expr test = new Expr("π");
+//        System.out.println(test.value(0));
+//        Expr test = new Expr("cos(π)");
+//        System.out.println(test.value(0));
+//        test = new Expr("e");
+//        System.out.println(test.value(0));
+//        test = new Expr("qp");
+//        System.out.println(test.value(0));
+//        test = new Expr("h1");
+//        System.out.println(test.value(0));
+
+        Expr test = new Expr("randInt(1,10)");
         System.out.println(test.value(0));
 //        System.out.println(Expr.constantsList);
 //        Field[] fields = Constants.class.getDeclaredFields();
